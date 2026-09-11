5464fd6fa0a4309f17734225876ded57f0b564be
haibotong <tonghaibo020@gmail.com>
2026-09-10 20:19:18 -0700

    Parallelize the per-pixel loop with OpenMP

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 FUNC := g++
 copt := -c 
 OBJ_DIR := ./bin/
-FLAGS := -O3 -lm -g -Werror
+FLAGS := -O3 -lm -g -Werror -fopenmp
 
 CPP_FILES := $(wildcard src/*.cpp)
 OBJ_FILES := $(addprefix $(OBJ_DIR),$(notdir $(CPP_FILES:.cpp=.obj)))
--- main.cpp
+++ main.cpp
@@ -46,8 +46,9 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b){
 }
 
 void refresh(Autonoma* c){
-   for(int n = 0; n<H*W; ++n) 
-   { 
+   #pragma omp parallel for schedule(dynamic, 16)
+   for(int n = 0; n<H*W; ++n)
+   {
       Vector ra = c->camera.forward+((double)(n%W)/W-.5)*((c->camera.right))+(.5-(double)(n/W)/H)*((c->camera.up));
       calcColor(&DATA[3*n], c, Ray(c->camera.focus, ra), 0);
    }
