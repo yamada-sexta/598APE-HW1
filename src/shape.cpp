fb8f8f88d8a365c558196675c4c0bf2d93766908
haibotong <tonghaibo020@gmail.com>
2026-09-10 20:19:18 -0700

    Rewrite calcColor as a single nearest-hit pass

--- src/shape.cpp
+++ src/shape.cpp
@@ -31,41 +31,18 @@ void Shape::setRoll(double c){
    zsin = sin(roll);
 }
 
-typedef struct {
-   double time;
-   Shape* shape;
-} TimeAndShape;
-
-void insertionSort(TimeAndShape *arr, int n) {
-    for (int i = 1; i < n; ++i) {
-        TimeAndShape key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j].time > key.time) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
 void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
-   ShapeNode* t = c->listStart;
-   TimeAndShape *times = (TimeAndShape*)malloc(0);
-   size_t seen = 0;
-   while(t!=NULL){
+   double curTime = inf;
+   Shape* curShape = NULL;
+   for(ShapeNode* t = c->listStart; t != NULL; t = t->next){
       double time = t->data->getIntersection(ray);
-
-      TimeAndShape *times2 = (TimeAndShape*)malloc(sizeof(TimeAndShape)*(seen + 1));
-      for (int i=0; i<seen; i++)
-         times2[i] = times[i];
-      times2[seen] = (TimeAndShape){ time, t->data };
-      free(times);
-      times = times2;
-      seen ++;
-      t = t->next;
+      if(time < curTime){
+         curTime = time;
+         curShape = t->data;
+      }
    }
-   insertionSort(times, seen);
-   if (seen == 0 || times[0].time == inf) {
+
+   if(curShape == NULL){
       double opacity, reflection, ambient;
       Vector temp = ray.vector.normalize();
       const double x = temp.x;
@@ -75,10 +52,6 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
       c->skybox->getColor(toFill, &ambient, &opacity, &reflection, fix(angle/M_TWO_PI),fix(me));
       return;
    }
-
-   double curTime = times[0].time;
-   Shape* curShape = times[0].shape;
-   free(times);
 
    Vector intersect = curTime*ray.vector+ray.point;
    double opacity, reflection, ambient;
